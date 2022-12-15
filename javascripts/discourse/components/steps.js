1c3b795b63bfec9636771e2fb2567c221cca2397
gal-samuel-israel <webmaster@agency.co.il>
2022-12-15 18:26:39 +0200

    simplify

--- javascripts/discourse/components/steps.js
+++ javascripts/discourse/components/steps.js
@@ -1,42 +1,15 @@
 import Component from "@glimmer/component";
-import { ajax } from "discourse/lib/ajax";
 import { tracked } from "@glimmer/tracking";
-import { getOwner } from "@ember/application";
 
 export default class Steps extends Component {
   @tracked steps = [];
 
   constructor() {
     super(...arguments);
-
     const count = this.args?.params?.count || 5;
-    var stepsArray = [];
-
-    /*
-    ajax(`/directory_items.json?period=yearly&order=likes_received`).then(
-      (data) => {
-        this.topContributors = data.directory_items.slice(0, count);
-      }
-    );
-    */  
-
-    if (getOwner(this).hasRegistration(`component:steps`)) {
-      console.log('hasRegistration');
-      stepsArray = [{id:'step-1',content:'<p>Step 1</p>'},{id:'step-2',content:'<p>Step 2</p>'}];
-
-    } else {
-      // eslint-disable-next-line no-console
-      console.warn(
-        `The component was not found`
-      );
-    }
-
-    this.steps = stepsArray;
+    this.steps = [{id:'step-1',content:'<p>Step 1</p>'},{id:'step-2',content:'<p>Step 2</p>'}];
 
   };
-
-  
-  
 
   willDestroy() {
     this.steps = null;
