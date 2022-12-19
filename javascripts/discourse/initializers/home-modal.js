f55c1087254da708f4c283cfa7633115cdc7e797
gal-samuel-israel <webmaster@agency.co.il>
2022-12-19 16:58:02 +0200

    hideModal on UserPreference

--- javascripts/discourse/initializers/home-modal.js
+++ javascripts/discourse/initializers/home-modal.js
@@ -64,7 +64,7 @@ export default {
             }
             
             if(isHome && !blockModal) {                          
-                showGate('home-modal');
+                if(!userHidePreference) { showGate('home-modal'); }
             }
           });
         
