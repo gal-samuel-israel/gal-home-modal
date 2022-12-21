35aa59caccd6e30d3230920de1442d4a22e71482
gal-samuel-israel <webmaster@agency.co.il>
2022-12-21 19:08:47 +0200

    add id='bio-editor'

--- javascripts/discourse/controllers/home-modal.js
+++ javascripts/discourse/controllers/home-modal.js
@@ -33,7 +33,7 @@ export default Ember.Controller.extend(ModalFunctionality, {
 
     ajax(`/u/${this.currentUser.username}.json`)
     .then((data) => {        
-        console.log(data);        
+        //console.log(data);        
         this.currentUser.set("bio_raw", data.user.bio_raw); 
         this.currentUser.set("bio_cooked", data.user.bio_cooked); 
         this.currentUser.set("bio_excerpt", data.user.bio_excerpt); 
