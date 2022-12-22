6b7a371aaac7a050dfa2ea1ca339336558be28ea
gal-samuel-israel <webmaster@agency.co.il>
2022-12-22 14:11:41 +0200

    send change event

--- javascripts/discourse/controllers/home-modal.js
+++ javascripts/discourse/controllers/home-modal.js
@@ -44,6 +44,7 @@ export default Ember.Controller.extend(ModalFunctionality, {
         this.newBioCooked = this.currentUser.bio_cooked;
 
         document.querySelector("textarea.d-editor-input").value = this.currentUser.bio_raw;
+        document.querySelector("textarea.d-editor-input").change();
         
         if(this.debugForAdmins){
           //console.log('d-editor-input set ok');          
