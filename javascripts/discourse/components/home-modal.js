400c1cdf389931ffb0a0c2e4bd715a717466b896
gal-samuel-israel <webmaster@agency.co.il>
2022-12-27 19:11:51 +0200

    debug handleTabKeyStrokes

--- javascripts/discourse/components/home-modal.js
+++ javascripts/discourse/components/home-modal.js
@@ -120,9 +120,7 @@ export default Component.extend({
       console.log(e);
     }    
     var KEYCODE_TAB = 9;
-
     var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-
     if (!isTabPressed) { 
       return; 
     }
@@ -131,22 +129,23 @@ export default Component.extend({
       console.log('document.activeElement:');
       console.log(document.activeElement);
     }
-
+    /*
     var arrFocusableElements = this.currentFocusableElements;
     var firstFocusableEl = arrFocusableElements[0];  
     var lastFocusableEl = arrFocusableElements[arrFocusableElements.length - 1];
 
-    if ( e.shiftKey ) /* shift + tab */ {
+    if ( e.shiftKey )  { //shift + tab 
       if (document.activeElement === firstFocusableEl) {
         lastFocusableEl.focus();
           e.preventDefault();
         }
-      } else /* tab */ {
+      } else  { //tab 
       if (document.activeElement === lastFocusableEl) {
         firstFocusableEl.focus();
           e.preventDefault();
         }
       }
+      */
   },
   
   //focus trap
