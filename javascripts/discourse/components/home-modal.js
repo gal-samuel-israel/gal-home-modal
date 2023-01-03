4a9e286ceef5e9b0f5320afac241bc8a708b29c2
gal-samuel-israel <webmaster@agency.co.il>
2023-01-03 20:14:18 +0200

    try another syntax

--- javascripts/discourse/components/home-modal.js
+++ javascripts/discourse/components/home-modal.js
@@ -143,7 +143,7 @@ export default Component.extend({
             console.log(userFields);
           }
           if(userFields[1]==='undefined' || userFields[1]===null){
-            this.currentUser.setProperties({'user_fields': {1: true}});        
+            this.currentUser.setProperties({"user_fields": {"1": true}});        
             return this.currentUser
               .save(["user_fields"])
               .then((resp) => {
