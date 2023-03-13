0df597b2a66ff4f119587a3bfaa5234d4f0dde35
gal-samuel-israel <webmaster@agency.co.il>
2023-03-13 11:48:28 +0200

    try decorateCookedElement

--- javascripts/discourse/api-initializers/init-home-modal.js
+++ javascripts/discourse/api-initializers/init-home-modal.js
@@ -37,20 +37,50 @@ export default apiInitializer("0.8", (api) => {
 
       //add hamburger custom link that will be used force the modal appearance 
       if(debug){
-        //did not work // mapRoutes('firstStepModal', (params)=>{ console.log('shoot', params);});
-        
-        console.log('testing api.decorateWidget'); 
-        api.decorateWidget('hamburger-menu:generalLinks', (helper) => {
-          //console.log('click', helper);                              
-          return {
-            href: "https://community.algosec.com/?force=1st-step",
-            //route: `discovery.${defaultHomepage()}`,
-            //mapRoutes did not work//route: 'firstStepModal',
-            //href: "",
-            className: "first-step-link",
-            rawLabel: "First Step",            
-          }
-        });        
+          //did not work // mapRoutes('firstStepModal', (params)=>{ console.log('shoot', params);});
+          
+          console.log('testing api.decorateWidget'); 
+          api.decorateWidget('hamburger-menu:generalLinks', (helper) => {
+            //console.log('click', helper);                              
+            return {
+              href: "https://community.algosec.com/?force=1st-step",
+              //route: `discovery.${defaultHomepage()}`,
+              //mapRoutes did not work//route: 'firstStepModal',
+              //href: "",
+              className: "first-step-link",
+              rawLabel: "First Step",            
+            }
+          }); 
+          
+          _firstStepClickHandler = function(event){
+            event.preventDefault();
+            console.log('_firstStepClickHandler');
+            //helper.widget.appEvents.on("page:changed", (data) => {
+            //  console.log('referrer:', document.referrer);
+            //  if(data.url==='/?force=1st-step'){window.location.reload();}
+            //});
+          };
+          
+          api.decorateCookedElement(
+            (element) => {
+              schedule("afterRender", () => {
+                console.log(element);
+
+                // add click event to the hamburger menu 
+                //document
+                //.querySelector('.first-step-link')
+                //.addEventListener("click", this._firstStepClickHandler);
+
+              });
+            },
+            {
+              onlyStream: true,
+              id: "main",
+            }
+          );
+
+            
+          
       }
 
       api.registerConnectorClass("above-site-header", "home-modal", {
