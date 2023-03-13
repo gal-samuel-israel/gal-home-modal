0c7f4d1dd66853d5e91cc79ca1157d5f32d03760
gal-samuel-israel <webmaster@agency.co.il>
2023-03-13 12:04:49 +0200

    try use anchor

--- javascripts/discourse/api-initializers/init-home-modal.js
+++ javascripts/discourse/api-initializers/init-home-modal.js
@@ -1,6 +1,4 @@
 import { apiInitializer } from "discourse/lib/api";
-import { defaultHomepage } from "discourse/lib/utilities";
-import { mapRoutes } from "discourse/mapping-router";
 
 export default apiInitializer("0.8", (api) => {
 
@@ -33,55 +31,7 @@ export default apiInitializer("0.8", (api) => {
     var isAdmin = (currentUser.admin)        
     blockModal = (showOnlyToAdmins && !isAdmin);
 
-    if(!blockModal){
-
-      //add hamburger custom link that will be used force the modal appearance 
-      if(debug){
-          //did not work // mapRoutes('firstStepModal', (params)=>{ console.log('shoot', params);});
-          
-          console.log('testing api.decorateWidget'); 
-          api.decorateWidget('hamburger-menu:generalLinks', (helper) => {
-            //console.log('click', helper);                              
-            return {
-              href: "https://community.algosec.com/?force=1st-step",
-              //route: `discovery.${defaultHomepage()}`,
-              //mapRoutes did not work//route: 'firstStepModal',
-              //href: "",
-              className: "first-step-link",
-              rawLabel: "First Step",            
-            }
-          }); 
-          
-          var _firstStepClickHandler = function(event){
-            event.preventDefault();
-            console.log('_firstStepClickHandler');
-            //helper.widget.appEvents.on("page:changed", (data) => {
-            //  console.log('referrer:', document.referrer);
-            //  if(data.url==='/?force=1st-step'){window.location.reload();}
-            //});
-          };
-          
-          api.decorateCookedElement(
-            (element) => {
-              schedule("afterRender", () => {
-                console.log(element);
-
-                // add click event to the hamburger menu 
-                //document
-                //.querySelector('.first-step-link')
-                //.addEventListener("click", this._firstStepClickHandler);
-
-              });
-            },
-            {
-              onlyStream: true,
-              id: "main",
-            }
-          );
-
-            
-          
-      }
+    if(!blockModal){      
 
       api.registerConnectorClass("above-site-header", "home-modal", {
         shouldRender() {
@@ -92,6 +42,24 @@ export default apiInitializer("0.8", (api) => {
       api.createWidget("home-modal-widget", {
         tagName: "div.home-modal",
       });
+
+      //add hamburger custom link that will be used force the modal appearance 
+      if(debug){
+        //did not work // mapRoutes('firstStepModal', (params)=>{ console.log('shoot', params);});
+        
+        console.log('testing api.decorateWidget'); 
+        api.decorateWidget('hamburger-menu:generalLinks', (helper) => {
+          console.log('helper:', helper);                              
+          return {
+            href: "https://community.algosec.com/?force=1st-step#force",            
+            //mapRoutes did not work//route: 'firstStepModal',
+            //href: "",
+            className: "first-step-link",
+            rawLabel: "First Step",            
+          }
+        });                         
+        
+    }
       
 
     }
