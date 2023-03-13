900d59e60bae4d637a05f536a7489687d508533d
gal-samuel-israel <webmaster@agency.co.il>
2023-03-13 12:43:35 +0200

    fix refreshTrapFocus

--- javascripts/discourse/components/home-modal.js
+++ javascripts/discourse/components/home-modal.js
@@ -284,6 +284,7 @@ export default Component.extend({
   
   //focus trap
   trapFocus(element) {
+    if(!element)
     var focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled]), [tabindex]:not([tabindex="-1"]');
     var newSet = false;
     if(!this.nodeListsAreEqual(this.currentFocusableElements, focusableEls)){
@@ -305,7 +306,7 @@ export default Component.extend({
   //Refresh the FocusTrap on steps change
   refreshTrapFocus(){
     var element = document.querySelector('#welcome-modal');
-    if(element !== 'undefined' && this.shouldDisplay && this.showModalPop){
+    if((element !== 'undefined' || element === null) && this.shouldDisplay && this.showModalPop){
       this.trapFocus(element);      
     }
   },
